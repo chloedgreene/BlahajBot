5bdc14a839e322e11c6ec3d80f7dbc625b1f8ebc
Chloe Greene <chloe@chloegreene.ca>
2024-04-23 09:57:47 -0400

    Added calling to path.txt for the path generated

--- src/main.cpp
+++ src/main.cpp
@@ -88,7 +88,9 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+    chassis.follow("path.txt",(60+30)*1000,3);
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
