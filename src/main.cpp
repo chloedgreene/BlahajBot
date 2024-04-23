6607bd4a08f286460b72815fb655123627b2a8da
Chloe Greene <chloe@chloegreene.ca>
2024-04-23 10:13:36 -0400

    Added On-screen telemetry

--- src/main.cpp
+++ src/main.cpp
@@ -53,6 +53,13 @@ void screen() {
         pros::lcd::print(1, "x: %f", pose.x); // print the x position
         pros::lcd::print(2, "y: %f", pose.y); // print the y position
         pros::lcd::print(3, "heading: %f", pose.theta); // print the heading
+        float average_temp = ((
+            ( left_front_motor.get_temperature())+
+            ( left_back_motor.get_temperature())+
+            (right_back_motor.get_temperature())+
+            (right_front_motor.get_temperature())
+            )/4);
+        pros::lcd::print(4, "Average Temprature: %f*C", pose.theta); // print the heading
         pros::delay(10);
     }
 }
@@ -66,56 +73,19 @@ void initialize() {
 
 void disabled() {}
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
 void competition_initialize() {}
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
 void autonomous() {
     chassis.follow("path.txt",(60+30)*1000,3);
 }
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
 		int left = master.get_analog(ANALOG_LEFT_Y);
 		int right = master.get_analog(ANALOG_RIGHT_Y);
-
-		
+		pros::lcd::print(5, "Input: %d %d", left,right);		
 
 		pros::delay(20);
 	}
